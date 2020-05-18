d0be329a64cd880595239e8f3fff67d5047f40b6
LeakingPipes <madsbull1@hotmail.com>
2020-05-18 14:13:09 +0200

    Chartsupdate

---
+++ SensorLogger/Models/ChartItemHolderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorLogger.Models
+{
+    public class ChartItemHolderModel
+    {
+        public string ValueType { get; set; }
+        public List<ChartItemModel> ChartItems{ get; set; }
+    }
+}
---
+++ SensorLogger/Models/ChartItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorLogger.Models
+{
+    public class ChartItemModel
+    {
+        public float Value { get; set; }
+        public string ValueType { get; set; }
+        public DateTime? Date_time { get; set; }
+    }
+}
